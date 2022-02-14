7e96735305accf2b42c3d10b0b7919eca88adcbe
MVPAlexLeDj <92936454+MVPAlexLeDj@users.noreply.github.com>
2022-02-14 14:27:22 +0100

    Update robotrading.py

--- robotrading.py
+++ robotrading.py
@@ -1,16 +1,20 @@
 class RoboTrading():
 
-    def __init__(self):
-        self.money = 100000
+    def __init__(self, client):
+        """
+        client is used to issue orders
+        """
+        self.client = client
 
     def process_candle(self, candle_msg:str):
         """This function is called when a new candle_msg is received.
             Candle message is a string of the form:
-            'symbol_key' : {'c': [174.3], 'h': [174.3], 'l': [174.19], 'o': [174.19], 's': 'ok', 't': [1643670000], 'v': [1888]}
+            {'symbol_key' : {'c': [174.3], 'h': [174.3], 'l': [174.19], 'o': [174.19], 's': 'ok', 't': [1643670000], 'v': [1888]}
 
             Note that there are list, so you can have multiple candles in one message.
         """
+        if self.client.money > 300 :
+            self.client.buy('AAPL', 5)
         
-
-    def gains(self):
-        return self.money
+    
+    
